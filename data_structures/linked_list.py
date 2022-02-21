1dfce9dbecfba627b1611f26faebe9d5a2979e1d
Kian Mehrabani <kianmehrabani@gmail.com>
2022-02-21 12:01:31 -0500

    add explicit inheritance

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -1,9 +1,10 @@
-from typing import Iterable, Iterator, List, Generic, Optional
+from typing import Iterable, Iterator, List, Optional
 
+from .list import List
 from .utils import T, Node
 
 
-class LinkedList(Generic[T]):
+class LinkedList(List[T]):
     def __init__(self, init: Optional[List[T]] = None) -> None:
         self.head: Optional[Node[T]] = None
         self.size: int = 0
