0794d425435e574926c46c6d6d2d342ab69fe289
Kian Mehrabani <kianmehrabani@gmail.com>
2022-05-08 23:51:45 -0400

    add cache

---
+++ data_structures/cache.py
@@ -0,0 +1,65 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from typing import Callable, Dict, Generic, Optional, Protocol, TypeVar, List
+
+from .utils import T
+
+
+K = TypeVar("K")
+V = TypeVar("V")
+
+
+class Cache(Protocol[K, V]):
+    capacity: int
+
+    @abstractmethod
+    def get(self, key: K, default: Optional[V] = None) -> Optional[V]:
+        ...
+
+    @abstractmethod
+    def put(self, key: K, value: V) -> None:
+        ...
+
+    def get_or_compute(self, key: K, supplier: Callable[[K], V]) -> V:
+        return self.get(key) or supplier(key)
+
+
+@dataclass
+class CacheValue(Generic[T]):
+    value: T
+    priority: int
+
+
+
+@dataclass
+class LRUCache(Cache[K, V]):
+    capacity: int
+    size: int = 0
+    queue: List[CacheValue[K]] = field(default_factory=list)
+    table: Dict[K, V] = field(default_factory=dict)
+
+    def get(self, key: K, default: Optional[V] = None) -> Optional[V]:
+        value = self.table.get(key, default)
+
+        if value is not None:
+            for cvalue in self.queue:
+                if cvalue.value == key:
+                    cvalue.priority += 1
+                    self.queue = list(sorted(self.queue, key=lambda x: x.priority))
+
+        return value
+
+    def put(self, key: K, value: V) -> None:
+        self.queue.append(CacheValue(value=key, priority=1))
+        self.queue = list(sorted(self.queue, key=lambda x: x.priority))
+        self.table[key] = value
+        self.size += 1
+
+        if self.size > self.capacity:
+            self._evict()
+
+    def _evict(self) -> CacheValue[K]:
+        cvalue = self.queue.pop(0)
+        del self.table[cvalue.value]
+
+        return cvalue
---
+++ tests/test_cache.py
@@ -0,0 +1,34 @@
+import unittest
+
+from data_structures.cache import Cache, LRUCache
+
+
+class LRUCacheTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.cache: Cache = LRUCache(10)
+
+    def test_get_after_put_under_capacity(self):
+        self.cache.put("a", 1)
+        self.assertEqual(1, self.cache.get("a"))
+        self.cache.put("b", 2)
+        self.assertEqual(2, self.cache.get("b"))
+        self.assertEqual(1, self.cache.get("a"))
+
+    def test_get_reflects_mutated_entry(self):
+        self.cache.put("a", 1)
+        self.cache.put("a", 10)
+        self.assertEqual(10, self.cache.get("a"))
+
+    def test_put_deletes_lru_when_full(self):
+        self.cache.put(0, 0)
+        for i in range(1, self.cache.capacity):
+            self.cache.put(i, i)
+            self.cache.get(i)
+
+        self.cache.put(self.cache.capacity + 1, self.cache.capacity + 1)
+        self.assertIsNone(self.cache.get(0))
+
+    def test_get_or_compute(self):
+        self.assertEqual(10, self.cache.get_or_compute("a", lambda k: 10))
+        self.cache.put("a", 1)
+        self.assertEqual(1, self.cache.get_or_compute("a", lambda k: 10))
